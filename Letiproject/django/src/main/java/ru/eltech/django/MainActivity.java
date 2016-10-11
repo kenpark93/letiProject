3cf52c0a2f7c9b3f3b325e93afc09902a372761b
Name <Name@mail.ru>
2016-10-11 17:30:51 +0300

    first commit

---
+++ Letiproject/django/src/main/java/ru/eltech/django/MainActivity.java
@@ -0,0 +1,23 @@
+package ru.eltech.django;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+    }
+
+    public void onClickStart(View v) {
+        textView.setText("Идет соединение...");
+    }
+}
